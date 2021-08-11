ec8a731f300fdcba2ba34b327c5afda2cb64551d
Umakant <umya08@gmail.com>
2021-08-11 20:50:10 +0530

    Display all questions

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,9 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('questions', [App\Http\Controllers\QuestionsController::class,'index']);
+
+//Route::resource('questions', 'QuestionsController');
+//Route::resource('questions', 'QuestionsController');
+//Route::resource('questions', QuestionsController::class);
